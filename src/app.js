dedbba328ac642850e7f93d765e9d68622496d39
seshasai-VVIT539 <seshasai8121@gmail.com>
2022-02-24 14:22:14 +0530

    authentication path

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
+        "Origin, X-Requested, Content-Type, Accept, Authorization"
     )
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     if (req.method === "OPTIONS") {
         res.header(
